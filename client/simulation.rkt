ee1a0aaac054f668470c475471126b0d74904d01
Eric Griffis <dedbox@gmail.com>
2018-11-04 10:34:58 -0800

    use x/y min/max calculators in collision detector

--- client/simulation.rkt
+++ client/simulation.rkt
@@ -25,26 +25,9 @@
            (when (ormap (curry sprites-collide? P) Ws)
              (set-sprite-y! P y)))))]))
 
-(define (sprites-collide? P W)
-  (match* (P W)
-    [((sprite P-bitmap P-x P-y)
-      (sprite W-bitmap W-x W-y))
-     (let* ([P-w/2 (fl/ (->fl (send P-bitmap get-width))  2.)]
-            [P-h/2 (fl/ (->fl (send P-bitmap get-height)) 2.)]
-            [W-w/2 (fl/ (->fl (send W-bitmap get-width))  2.)]
-            [W-h/2 (fl/ (->fl (send W-bitmap get-height)) 2.)]
-            ;; ------------------------
-            [P-x-min (fl- P-x P-w/2)]
-            [P-x-max (fl+ P-x P-w/2)]
-            [P-y-min (fl- P-y P-h/2)]
-            [P-y-max (fl+ P-y P-h/2)]
-            [W-x-min (fl- W-x W-w/2)]
-            [W-x-max (fl+ W-x W-w/2)]
-            [W-y-min (fl- W-y W-h/2)]
-            [W-y-max (fl+ W-y W-h/2)]
-            ;; ------------------------
-            [d1x (fl- W-x-min P-x-max)]
-            [d1y (fl- W-y-min P-y-max)]
-            [d2x (fl- P-x-min W-x-max)]
-            [d2y (fl- P-y-min W-y-max)])
-       (and (fl> 0. d1x) (fl> 0. d1y) (fl> 0. d2x) (fl> 0. d2y)))]))
+(define (sprites-collide? sprite1 sprite2)
+  (let ([d1x (fl- (sprite-x-min sprite2) (sprite-x-max sprite1))]
+        [d1y (fl- (sprite-y-min sprite2) (sprite-y-max sprite1))]
+        [d2x (fl- (sprite-x-min sprite1) (sprite-x-max sprite2))]
+        [d2y (fl- (sprite-y-min sprite1) (sprite-y-max sprite2))])
+    (and (fl> 0. d1x) (fl> 0. d1y) (fl> 0. d2x) (fl> 0. d2y))))
