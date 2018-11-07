548d585ea55e152570aad61a53004b95aeb58416
Eric Griffis <dedbox@gmail.com>
2018-11-06 21:41:39 -0800

    move sprites-collide? into sprite module

--- client/simulation.rkt
+++ client/simulation.rkt
@@ -24,10 +24,3 @@
            (set-sprite-x! player1 x)
            (when (ormap (curry sprites-collide? player1) walls)
              (set-sprite-y! player1 y)))))]))
-
-(define (sprites-collide? sprite1 sprite2)
-  (let ([d1x (fl- (sprite-x-min sprite2) (sprite-x-max sprite1))]
-        [d1y (fl- (sprite-y-min sprite2) (sprite-y-max sprite1))]
-        [d2x (fl- (sprite-x-min sprite1) (sprite-x-max sprite2))]
-        [d2y (fl- (sprite-y-min sprite1) (sprite-y-max sprite2))])
-    (and (fl> 0. d1x) (fl> 0. d1y) (fl> 0. d2x) (fl> 0. d2y))))
--- client/sprite.rkt
+++ client/sprite.rkt
@@ -40,3 +40,10 @@
 
 (define (sprite-y-max S)
   (fl+ (sprite-y S) (fl/ (->fl (send (sprite-bitmap S) get-height)) 2.)))
+
+(define (sprites-collide? sprite1 sprite2)
+  (let ([d1x (fl- (sprite-x-min sprite2) (sprite-x-max sprite1))]
+        [d1y (fl- (sprite-y-min sprite2) (sprite-y-max sprite1))]
+        [d2x (fl- (sprite-x-min sprite1) (sprite-x-max sprite2))]
+        [d2y (fl- (sprite-y-min sprite1) (sprite-y-max sprite2))])
+    (and (fl> 0. d1x) (fl> 0. d1y) (fl> 0. d2x) (fl> 0. d2y))))
